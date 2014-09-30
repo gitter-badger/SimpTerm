168e1647f359087cf78a57ebb52b11b94c68458d
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-09-30 14:17:19 +0200

    Working terminal.

--- term.py
+++ term.py
@@ -1,10 +1,11 @@
 'SimpTerm'
+import subprocess
 class AppInfo():
     version = "1.0.0.0"
 class Configuration():
     init_onstartup = False
 app = AppInfo()
-config = COnfiguration()
+config = Configuration()
 def init():
     print("SimpTerm v" + app.version)
     print()
@@ -12,7 +13,15 @@ def init():
     term()
 def term():
     term_input = input("$>")
+    if term_input == "": term()
+    elif term_input == "about": About()
+    else:
+        subprocess.Popen(term_input)
+        term()
+def About():
+    print("SimpTerm v" + app.version)
 def read_config():
     if config.init_onstartup == True: init()
     else: term()
+'Reads the configuration'
 read_config()
