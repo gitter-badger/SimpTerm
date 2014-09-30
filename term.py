6c2b1dc4e73398b53812dcbd4bafb0dd18ce6ac5
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-09-30 14:10:03 +0200

    Some lines of code.

--- term.py
+++ term.py
@@ -1,1 +1,18 @@
 'SimpTerm'
+class AppInfo():
+    version = "1.0.0.0"
+class Configuration():
+    init_onstartup = False
+app = AppInfo()
+config = COnfiguration()
+def init():
+    print("SimpTerm v" + app.version)
+    print()
+    print("")
+    term()
+def term():
+    term_input = input("$>")
+def read_config():
+    if config.init_onstartup == True: init()
+    else: term()
+read_config()
