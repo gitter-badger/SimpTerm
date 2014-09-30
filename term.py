2dfce10d7fc4389682d53be8584a91b9a4c96dcb
Tristan B. Kildaire <tristankildaire@gmail.com>
2014-09-30 14:42:29 +0200

    Updater added.
    Includes two methods of updating:
    
    * Wget method
    * Git method

--- term.py
+++ term.py
@@ -4,6 +4,7 @@ class AppInfo():
     version = "1.0.0.0"
 class Configuration():
     init_onstartup = False
+    update_using_git = False
 app = AppInfo()
 config = Configuration()
 def init():
@@ -15,12 +16,32 @@ def term():
     term_input = input("$>")
     if term_input == "": term()
     elif term_input == "exit": exit()
-    elif term_input == "about": About()
+    elif term_input == "st -about": About()
+    elif term_input == "st -update": Updates()
     else:
         subprocess.Popen(term_input)
         term()
 def About():
     print("SimpTerm v" + app.version)
+    term()
+def Updates():
+    print("SimpTerm v" + app.version)
+    print()
+    if config.update_using_git == True:
+        print("Getting updates...")
+        print("Fetching files... [0%]")
+        subprocess.Popen("git clone https://github.com/deavmi/SimpTerm.git")
+        print("Fetching files... [100%]")
+        print("Update completed!")
+    else:
+        print("Getting updates...")
+        print("Fetching file [1 of 3]")
+        subprocess.Popen("wget https://raw.githubusercontent.com/deavmi/SimpTerm/master/term.py")
+        print("Fetching file [2 of 3]")
+        subprocess.Popen("wget https://raw.githubusercontent.com/deavmi/SimpTerm/master/LICENSE")
+        print("Fetching file [3 of 3]")
+        subprocess.Popen("wget https://raw.githubusercontent.com/deavmi/SimpTerm/master/README.md")
+        print("Update completed!")
     term()
 def read_config():
     if config.init_onstartup == True: init()
